9ec144dd7cb959f4cff0dc07c2a2aa01357485d0
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2023-01-10 14:32:21 -0800

    add lesson 2 solution

--- 02_DOM_Manipulation/src/index.js
+++ 02_DOM_Manipulation/src/index.js
@@ -5,13 +5,36 @@ function formatPrice(price) {
   return '$' + Number.parseFloat(price).toFixed(2);
 }
 
+function renderHeader(bookStore) {
+  document.querySelector('#bookstore-name').textContent = bookStore.name;
+}
+
+renderHeader(bookStore);
+
+function renderFooter(bookStore) {
+  document.querySelector('#store').textContent = bookStore.location;
+  document.querySelector('#number').textContent = bookStore.number;
+  document.querySelector('#address').textContent = bookStore.address;
+}
+
+renderFooter(bookStore);
+
 // create a function called renderBook(book)
 // it will take a book object as an argument
 // and create the html struture for rendering 
 // that book and insert it into our webpage!
 
-// function renderBook(book) {
-// should create an li element that looks something like this:
+// {
+//   id: 1,
+//   title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+//   author: 'Marjin Haverbeke',
+//   price: 10.00,
+//   reviews: [{userID: 1, content:'Good book, but not great for new coders'}],
+//   inventory: 10,
+//   imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg'
+// }
+function renderBook(book) {
+  // should create an li element that looks something like this:
   // <li class="list-li">
   //   <h3>Eloquent JavaScript : A Modern Introduction to Programming</h3>
   //   <p>Marjin Haverbeke</p>
@@ -19,5 +42,35 @@ function formatPrice(price) {
   //   <img src="https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" alt="Eloquent JavaScript cover"/>
   //   <button>Delete</button>
   // </li>
+  const li = document.createElement("li");
+  li.className = "list-li";
 
+  const h3 = document.createElement("h3");
+  h3.textContent = book.title;
+  li.append(h3);
   
+  const pAuthor = document.createElement("p");
+  pAuthor.textContent = book.author;
+  li.append(pAuthor);
+  
+  const pPrice = document.createElement("p");
+  pPrice.textContent = formatPrice(book.price);
+  li.append(pPrice);
+
+  const img = document.createElement("img");
+  img.src = book.imageUrl;
+  img.alt = `${book.title} cover`;
+  img.title = `${book.title} cover`;
+  li.append(img);
+
+  const button = document.createElement("button");
+  button.textContent = 'Delete';
+  li.append(button);
+
+  document.querySelector('#book-list').append(li);
+}
+  
+const book = bookStore.inventory[0]
+bookStore.inventory.forEach(book => {
+  renderBook(book);
+})
